f167663f45747b20686fd36e097a61d24dc65646
ptrkc <40273827+ptrkc@users.noreply.github.com>
2021-06-16 22:19:11 -0300

    Added GET /categories

--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import cors from "cors";
 import pg from "pg";
 import dbConfig from "./dbConfig.js";
 
+const db = new pg.Pool(dbConfig);
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const db = new pg.Pool(dbConfig);
+app.get("/categories", async (req, res) => {
+    try {
+        const games = await db.query("SELECT * FROM categories");
+        res.send(games.rows);
+    } catch (e) {
+        res.sendStatus(500);
+    }
+});
 
 app.listen(4000, () => {
     console.log("Server started on port 4000.");
